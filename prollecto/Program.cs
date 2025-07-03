d959bbb46b6849b1e8e42b60de3e159c561e49cf
JAIRO ALEJANDRO VALDOVINOS DAMIAN <22460706@colima.tecnm.mx>
2025-07-03 10:43:56 -0600

    cambio

--- prollecto/Program.cs
+++ prollecto/Program.cs
@@ -14,6 +14,7 @@ namespace prollecto
             Console.WriteLine("holis ;v hola");
             Console.WriteLine("holaaaa ivanaaaa");
             Console.WriteLine("xd");
+            Console.WriteLine("jairo 20 años");
         }
     }
 }
