9b00fe6bdbf2de999298dbcec0fdfb3be681135d
AlexHyva <abj1717@gmail.com>
2025-07-03 10:44:08 -0600

    5

--- prollecto/Program.cs
+++ prollecto/Program.cs
@@ -9,6 +9,18 @@ namespace prollecto
             Console.WriteLine("Hola profe");
             Console.WriteLine("ya quedo profe");
             Console.WriteLine("hello");
+            Console.WriteLine("Hello World!");      
+                
+                
+                
+                
+                
+            Console.WriteLine("Hello World! from prollecto");
+            Console.WriteLine("Hello World! from prollecto 2");
+            Console.WriteLine("Hello World! from prollecto 3");
+            Console.WriteLine("Hello World! from prollecto 4");
+            Console.WriteLine("Hello World! from prollecto 5");
+                
         }
     }
 }
