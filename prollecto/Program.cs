151487239573eadcad0ef94fe227fe8ca067f0e8
Ramiro Valdovinos <edgaritoram2@gmail.com>
2025-07-03 10:34:23 -0600

    hola

--- prollecto/Program.cs
+++ prollecto/Program.cs
@@ -9,6 +9,8 @@ namespace prollecto
             Console.WriteLine("Hola profe");
             Console.WriteLine("ya quedo profe");
             Console.WriteLine("hello");
+
+            Console.WriteLine("hola Ivanna");
         }
     }
 }
